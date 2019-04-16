ee7d1ee610fd5251d4387fefc1e0c2971c6f4cb6
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-16 15:13:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma talk
     "HUH?! SPEAK UP, SONNY!"
   elsif talk.upcase
     "NO, NOT SINCE 1938!"
-  elsif talk == "I LOVE YOU GRANDMA"
+  elsif talk.upcase == "I LOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
   end
 end
